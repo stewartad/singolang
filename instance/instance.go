e9b94a419f08516ae1b5478f6b6bad4d5b34a3ee
Austen Stewart <adstewart@email.arizona.edu>
2019-06-14 13:35:51 -0400

    some instance functions

--- instance/instance.go
+++ instance/instance.go
@@ -1,13 +1,37 @@
 package instance
 
+import (
+	"github.com/stewartad/singolang/utils"
+	"fmt"
+)
+
 type Instance struct {
 	name string
+	imageURI string
+	protocol string
+	image string
+	metadata []string
 }
 
-func (i *Instance) Start() {
-
+func (i *Instance) String() string {
+	if i.protocol != "" {
+		return fmt.Sprintf("%s:\\%s", i.protocol, i.image)
+	}
+	return i.image
 }
 
-func (i *Instance) Stop() {
+func GetInstance(image string) *Instance {
+	i := new(Instance)
+	i.ParseImageName(image)
+
+	return i
+}
+
+func (i *Instance) ParseImageName(image string) {
+	i.imageURI = image
+	i.protocol, i.image = utils.SplitURI(image)
+}
+
+func (i *Instance) updateMetadata() {
 
 }
