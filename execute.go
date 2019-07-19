d6e97181f7b13ec07b5921e01fe5b79faeeb7fb6
Austen Stewart <adstewart@email.arizona.edu>
2019-07-19 11:32:22 -0900

    logging

--- execute.go
+++ execute.go
@@ -3,6 +3,7 @@ package singolang
 import (
 	"fmt"
 	"strings"
+	"log"
 )
 
 // ExecOptions provide flags simulating options int he singularity command line
@@ -72,7 +73,7 @@ func (i *Instance) execute(command []string, opts *ExecOptions, sudo bool) (stri
 	cmd = append(cmd, image)
 	cmd = append(cmd, command...)
 
-	fmt.Println(cmd)
+	log.Println(cmd)
 
 	stdout, stderr, status, err := runCommand(cmd, &runCommandOptions{
 		sudo:     sudo,
